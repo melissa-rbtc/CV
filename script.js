9c16ad574a8c40c835c66b7699ad9f786676f0de
Melissa-mrc <melissa.rbtc@outlook.com>
2021-07-09 20:07:17 +0200

    Ajout Fonction compteur live

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ var typed = new Typed('.typed', {
   strings : ["Bonjour à tous et bienvenue sur ma page, Je me présente je m'appelle Mélissa", "Étudiante motivée en reconversion professionnelle à la suite d'une reconnaissance de la qualité de travailleur handicapé (RQTH). Je suis actuellement à la recherche d'une entreprise pour un contrat en alternance avec Openclassrooms formation développeur Web – Titre RNCP – niveau BAC +2 de 12 mois au rythme de 4-5 jours entreprise, 1-2 jours formation. Dotée d'une grande capacité d'adaptation, je sais me montrer proactive et digne de confiance dans la réalisation de mes missions."],
   typeSpeed: 25,
 });
+
+// COMPTEUR LIVE
+
+let compteur = 0;
+
+$(window).scroll(function() {
+
+  const top = $('.counter').offset().top - window.innerHeight;
+
+  if (compteur == 0 && $(window).scrollTop() > top) {
+    $('.counter-value').each(function() {
+      let $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum : countTo
+      },
+      {
+        duration: 10000,
+        easing: 'swing',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+      });
+    });
+    compteur = 1;
+  }
+});
